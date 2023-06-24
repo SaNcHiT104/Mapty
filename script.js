c8785b0099a30cd5e1143d2e9c046a46beeb8e88
SaNcHiT104 <jainsanchit14112002@gmail.com>
2023-06-24 23:04:09 +0530

    form rendered

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+//global variables
+//1-> map object
+//2-> location object
+
+let map, mapEvent;
 //setting up geoloaction (one fucntion  for successful, 2nd for error)
 
 navigator.geolocation.getCurrentPosition(
@@ -19,7 +24,7 @@ navigator.geolocation.getCurrentPosition(
     const { longitude } = position.coords;
     const coords = [latitude, longitude];
     //map is class id and L is leaflet
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
     //fr/hot/
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
@@ -27,27 +32,47 @@ navigator.geolocation.getCurrentPosition(
     }).addTo(map);
 
     //on just like addEventListener in leaflet
-    map.on('click', function (mapEvent) {
+    map.on('click', function (mapE) {
+      //rendering form
+      mapEvent = mapE; //assigning clicked coordinates
+      form.classList.remove('hidden');
+      //adding cursor to distance
+      inputDistance.focus();
       //   console.log(mapEvent);
-      const { lat, lng } = mapEvent.latlng;
 
       //adding marker
-      L.marker({ lat, lng })
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('Runner')
-        .openPopup();
+      //after form is clicked
     });
   },
   function () {
     alert('Could not get your position');
   }
 );
+
+//form submit button is clicked, so marker should be adjusted
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  //marker is added
+  const { lat, lng } = mapEvent.latlng;
+  L.marker({ lat, lng })
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
