f6c42e225ad5e15a917158c426a688cf3eaa0c67
SaNcHiT104 <jainsanchit14112002@gmail.com>
2023-06-24 23:57:23 +0530

    refracting of code done, class App added

--- script.js
+++ script.js
@@ -11,74 +11,83 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-//global variables
-//1-> map object
-//2-> location object
+// getpostion-->loadMap-->click-->showForm-->form Submit-->new Workout (flow)
+//setting up classes
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPostion();
+    //form submit button is clicked, so marker should be adjusted
+    form.addEventListener('submit', this._newWorkout.bind(this));
 
-let map, mapEvent;
-//setting up geoloaction (one fucntion  for successful, 2nd for error)
-
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+    //toggling hidden class for runner and cycling
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
+  }
+  //basically checks for location access and pass the access to load Map
+  _getPostion() {
+    //setting up geoloaction (one function  for successful, 2nd for error)
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this),
+      function () {
+        alert('Could not get your position');
+      }
+    );
+  }
+  _loadMap(position) {
     const { latitude } = position.coords; //position->object(coords in which latitude and longitude is present)
     const { longitude } = position.coords;
     const coords = [latitude, longitude];
     //map is class id and L is leaflet
-    map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, 13);
     //fr/hot/
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
     //on just like addEventListener in leaflet
-    map.on('click', function (mapE) {
-      //rendering form
-      mapEvent = mapE; //assigning clicked coordinates
-      form.classList.remove('hidden');
-      //adding cursor to distance
-      inputDistance.focus();
-      //   console.log(mapEvent);
-
-      //adding marker
-      //after form is clicked
-    });
-  },
-  function () {
-    alert('Could not get your position');
+    this.#map.on('click', this._showForm.bind(this)); //.bind this is used because when we use this on event listener this points to here(this.#map) not on objects
   }
-);
+  _showForm(mapE) {
+    //rendering form
+    this.#mapEvent = mapE; //assigning clicked coordinates
+    form.classList.remove('hidden');
+    //adding cursor to distance
+    inputDistance.focus();
+    //adding marker after form is clicked
+  }
 
-//form submit button is clicked, so marker should be adjusted
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-  //clear input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
+  _newWorkout(e) {
+    e.preventDefault();
 
-  //marker is added
-  const { lat, lng } = mapEvent.latlng;
-  L.marker({ lat, lng })
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
+    //clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
 
-//toggling hidden class for runner and cycling
-inputType.addEventListener('change', function () {
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-});
+    //marker is added
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker({ lat, lng })
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
+const app = new App();
