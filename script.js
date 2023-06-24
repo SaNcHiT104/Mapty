c26a726f5fe91afc9ed1a9992be7a6e38bec6916
SaNcHiT104 <jainsanchit14112002@gmail.com>
2023-06-24 21:51:23 +0530

    geolocation added

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//setting up geoloaction (one fucntion  for successful, 2nd for error)
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords; //position->object(coords in which latitude and longitude is present)
+    const { longitude } = position.coords;
+    console.log(
+      `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`
+    );
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);
