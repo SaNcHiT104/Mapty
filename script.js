475e59bd31d5855761f4678fc669759eae463028
SaNcHiT104 <jainsanchit14112002@gmail.com>
2023-06-25 13:30:02 +0530

    push

--- script.js
+++ script.js
@@ -173,11 +173,6 @@ class App {
     }
     //clearing the form
     // Add new object to workout array
-    inputCadence.value =
-      inputDistance.value =
-      inputDuration.value =
-      inputElevation.value =
-        '';
     this.#workouts.push(workout);
 
     // Render workout on map as marker
